691b415a00b5c051d84f9ce93fe4bec96f0e34e4
Cody Wahl <Cody.Wahl@greenwayhealth.com>
2015-03-03 16:04:31 -0500

    Repository init

---
+++ 203_Easy/203_Easy/EasySquare.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace _203_Easy
+{
+    partial class EasySquare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.X = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // X
+            // 
+            this.X.AutoSize = true;
+            this.X.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.X.Location = new System.Drawing.Point(89, 64);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(14, 13);
+            this.X.TabIndex = 0;
+            this.X.Text = "X";
+            this.X.Click += new System.EventHandler(this.X_Click);
+            // 
+            // EasySquare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 162);
+            this.Controls.Add(this.X);
+            this.Name = "EasySquare";
+            this.Text = "EasySquare";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.EasySquare_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label X;
+    }
+}
---
+++ 203_Easy/203_Easy/EasySquare.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace _203_Easy
+{
+    public partial class EasySquare : Form
+    {
+        #region Constants
+
+        //Form Move data members
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+
+        private const int HT_CAPTION = 0x2;
+
+                    private Random r = new Random();
+
+        #endregion Constants
+
+        #region Form Movement
+
+        [DllImportAttribute("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd,
+        int Msg, int wParam, int lParam);
+
+        [DllImportAttribute("user32.dll")]
+        private static extern bool ReleaseCapture();
+
+        #endregion Form Movement
+
+        #region Constructor
+
+        public EasySquare()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+        }
+
+        #endregion Constructor
+
+        #region Private Helpers
+
+        private void SetNewBackgroundColor()
+        {
+            this.BackColor = Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+        }
+
+        private void SetNewSize()
+        {
+            int size = r.Next(50, 1000);
+            this.Size = new Size(size,size);
+            AdjustTheXsPosition(size);
+        }
+
+        private void AdjustTheXsPosition(int formSize)
+        {
+            int location = (formSize / 2) - (X.Width / 2);
+            X.Location = new Point(location, location);
+        }
+
+        #endregion Private Helpers
+
+        #region Event Handler
+
+        private void EasySquare_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                
+                SetNewBackgroundColor();
+                SetNewSize();
+            }
+        }
+
+        #endregion Event Handler
+
+        private void X_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
